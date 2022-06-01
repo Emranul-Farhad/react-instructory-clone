c038822f97341ecfae117e2cc312a2710d78a5cf
unknown <emranulfarhad@gmail.com>
2022-06-02 02:41:44 +0600

    admin seperate done

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
+import {RiAdminFill} from 'react-icons/ri'
+import {FiUsers} from "react-icons/fi"
+import {FcPackage} from 'react-icons/fc'
+
 
 const Dashboard = () => {
     return (
         <div>
+
             <div className='grid grid-cols-4'>
-                <NavLink class="btn btn-secondary" to='/dashboard/users' >Users</NavLink>
-                <NavLink class="btn btn-secondary" to='/dashboard/admins' >Admins</NavLink>
-                <NavLink class="btn bg-[#0076a3]"  to='/dashboard' >All orders</NavLink>
-                <NavLink class=" bg-[#0076a3]"  to='/dashboard' >All Teacher</NavLink>
+             <div className='flex flex-row justify-center items-center text-white font-bold uppercase mt-2 bg-[#0076a3]'> 
+               <NavLink className=" p-3 ms-2 " to='/dashboard/users' >Users
+                </NavLink>
+                {<FiUsers className='text-3xl' ></FiUsers>}
+               </div>
+
+             <div className='ms-2 flex flex-row justify-center items-center text-white font-bold uppercase mt-2 bg-[#0076a3]'>
+             <NavLink to='/dashboard/admins' >Admins</NavLink>
+             <RiAdminFill className='text-3xl'></RiAdminFill>
+             </div>
+
+            <div className='ms-2 flex flex-row justify-center items-center text-white font-bold uppercase mt-2 bg-[#0076a3]'>
+            <NavLink to='/dashboard' >All orders</NavLink>
+            <FcPackage className='text-3xl ms-1'></FcPackage>
+            </div>
+
+                <NavLink className=" p-3 ms-2 text-white font-bold uppercase mt-2  bg-[#0076a3]"  to='/dashboard' >All Teacher</NavLink>
+                
             </div>
             <div>
                 <Outlet></Outlet>
