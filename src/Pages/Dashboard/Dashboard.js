4d5c731433642534310baacd43a0fa09447381f3
unknown <emranulfarhad@gmail.com>
2022-06-02 13:04:05 +0600

    nav bar

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -3,32 +3,22 @@ import { NavLink, Outlet } from 'react-router-dom';
 import {RiAdminFill} from 'react-icons/ri'
 import {FiUsers} from "react-icons/fi"
 import {FcPackage} from 'react-icons/fc'
+import './Dashboard.css'
 
 
 const Dashboard = () => {
     return (
         <div>
 
-            <div className='grid grid-cols-4'>
-             <div className='flex flex-row justify-center items-center text-white font-bold uppercase mt-2 bg-[#0076a3]'> 
-               <NavLink className=" p-3 ms-2 " to='/dashboard/users' >Users
-                </NavLink>
-                {<FiUsers className='text-3xl' ></FiUsers>}
-               </div>
+<nav className='navd' >
+   <NavLink to='/dashboard/users'> <RiAdminFill className='text-black'></RiAdminFill> </NavLink>
+   <NavLink to='/dashboard/admins'> <FiUsers></FiUsers> </NavLink>
+   <NavLink to='/dashboard'> <FcPackage></FcPackage> </NavLink>
+   <NavLink to='/dashboard'> </NavLink>
+ </nav>
+  
 
-             <div className='ms-2 flex flex-row justify-center items-center text-white font-bold uppercase mt-2 bg-[#0076a3]'>
-             <NavLink to='/dashboard/admins' >Admins</NavLink>
-             <RiAdminFill className='text-3xl'></RiAdminFill>
-             </div>
 
-            <div className='ms-2 flex flex-row justify-center items-center text-white font-bold uppercase mt-2 bg-[#0076a3]'>
-            <NavLink to='/dashboard' >All orders</NavLink>
-            <FcPackage className='text-3xl ms-1'></FcPackage>
-            </div>
-
-                <NavLink className=" p-3 ms-2 text-white font-bold uppercase mt-2  bg-[#0076a3]"  to='/dashboard' >All Teacher</NavLink>
-                
-            </div>
             <div>
                 <Outlet></Outlet>
             </div>
@@ -37,3 +27,47 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
+
+
+{/* <div className='flex flex-col w-[100px]'>
+<div className='flex flex-row justify-center items-center text-white font-bold uppercase mt-2 bg-[#0076a3]'> 
+  <NavLink className=" p-3 ms-2 " to='/dashboard/users' >Users
+   </NavLink>
+   {<FiUsers className='text-3xl' ></FiUsers>}
+  </div>
+
+<div className='ms-2 flex flex-row justify-center items-center text-white font-bold uppercase mt-2 bg-[#0076a3]'>
+<NavLink to='/dashboard/admins' >Admins</NavLink>
+<RiAdminFill className='text-3xl'></RiAdminFill>
+</div>
+
+<div className='ms-2 flex flex-row justify-center items-center text-white font-bold uppercase mt-2 bg-[#0076a3]'>
+<NavLink to='/dashboard' >All orders</NavLink>
+<FcPackage className='text-3xl ms-1'></FcPackage>
+</div>
+
+   <NavLink className=" p-3 ms-2 text-white font-bold uppercase mt-2  bg-[#0076a3]"  to='/dashboard' >All Teacher</NavLink>
+   <NavLink className=" p-3 ms-2 text-white font-bold uppercase mt-2  bg-[#0076a3]"  to='/dashboard' >All Teacher</NavLink>
+   <NavLink className=" p-3 ms-2 text-white font-bold uppercase mt-2  bg-[#0076a3]"  to='/dashboard' >All Teacher</NavLink>
+
+</div> */}
+
+
+
+{/* <div class= 'container'> 
+   
+  
+  <section id= 'second'>
+    <h6>Second</h6>
+  </section>
+  
+ <section id= 'third'>
+   <h6>Third</h6>
+  </section>
+  
+ <section id= 'fourth'>
+   <h6>Fourth lorem50
+    amder desh tara koto vakl pay ami </h6>
+  </section>
+
+</div> */}
