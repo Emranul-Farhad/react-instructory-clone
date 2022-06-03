be816391600eb46c0acd651d12fb08a8d474da29
unknown <emranulfarhad@gmail.com>
2022-06-03 23:15:33 +0600

    rating update

--- src/components/Take Review/Takereview.js
+++ src/components/Take Review/Takereview.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from '../navbar/Nav';
+import { Rating } from '@mui/material';
+
+
+
 
 const Takereview = () => {
+
+    //    const rating handel
+    const [value, setValue] = useState(2);
+
+
     return (
         <div>
             <Nav></Nav>
@@ -12,17 +21,24 @@ const Takereview = () => {
                     <div class="card-body bg-[#ebe2e28e] mt-3">
                         <h6 className=' font-bold  text-[#0076a3] text-left mx-3 mb-2 '> Course Name </h6>
 
-                        <input type="text" placeholder="Type here" className="supportinput h-[50px] bg-[#FBFBFB] border-solid  p-2
-    input-bordered input-primary w-full max-w-xs mb-2"
+                        <input type="text" placeholder="Type here" className="supportinput h-[50px] bg-[#FBFBFB] border-solid  p-2 input-bordered input-primary w-full max-w-xs mb-2"
                         />
 
                         <h6 className='text-[#0076a3] font-bold text-left mx-3 mt-3'> Course description </h6>
-                        <textarea className='mt-2 supportinput h-[50px] bg-[#FBFBFB] border-solid  p-2
-     input-bordered input-primary w-full max-w-xs' name="" id="" cols="30"
+                        <textarea className='mt-2 supportinput h-[50px] bg-[#FBFBFB] border-solid  p-2 input-bordered input-primary w-full max-w-xs' name="" id="" cols="30"
                             rows="10"></textarea>
 
-                        <div class="card-actions justify-end">
-                            <button class="btn btn-primary">Buy Now</button>
+                        <Rating
+                        className='mr-[200px] mt-3'
+                            name="simple-controlled"
+                            value={2}
+                            onChange={(event, newValue) => {
+                                setValue(newValue);
+                            }}
+                        />
+
+                        <div class="card-actions justify-end mt-3 ">
+                            <button class="btn btn-primary">Submit ✔️ </button>
                         </div>
                     </div>
                 </div>
@@ -35,24 +51,3 @@ export default Takereview;
 
 
 
-// <div className='flex justify-center mt-5'>
-// <div class="card card-compact w-96 bg-base-100 shadow-xl">
-// <h6 className='text-[#0076a3] font-bold text-1xl mt-3'>Feel free to  Give your Review 😇</h6>
-// <div class="card-body bg-[#ebe2e28e] mt-3">
-
-//     <h6 className=' font-bold  text-[#0076a3] text-left mx-3 mb-2 '> Course Name </h6>
-// <input type="text" placeholder="Type here" className="supportinput h-[50px] bg-[#FBFBFB] border-solid  p-2
-//      input-bordered input-primary w-full max-w-xs mb-2"
-//      />
-
-//      <h6 className='text-[#0076a3] font-bold text-left mx-3 mt-3'> Course description </h6>
-//      <textarea className='mt-2 supportinput h-[50px] bg-[#FBFBFB] border-solid  p-2
-//      input-bordered input-primary w-full max-w-xs' name="" id="" cols="30"
-//       rows="10"></textarea>
-
-//     <div class="card-actions justify-end">
-//         <button class="btn btn-primary">Buy Now</button>
-//     </div>
-// </div>
-// </div>
-// </div>
