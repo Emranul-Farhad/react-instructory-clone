d600f9024bd70f55b38035426dd52f3668739124
unknown <emranulfarhad@gmail.com>
2022-06-03 16:47:09 +0600

    course upload section working

--- src/components/Courseupload/Courseupload.js
+++ src/components/Courseupload/Courseupload.js
@@ -1,32 +1,99 @@
 import React from 'react';
 import Nav from '../navbar/Nav';
-import {BsCheckLg} from 'react-icons/bs'
 import {AiFillCheckCircle} from 'react-icons/ai'
+
 
 const Courseupload = () => {
 
     return (
-    
         <div>
             <Nav></Nav>
-            <div>
-                <div>
-                    <h6>
-                    How you want to sell your course tutorial?
-                    </h6>
-                    <h6> If we market your course, definitely you will get better sells. But the decision is yours. </h6>
-                    <div>
-                        <div>
-                            <h6> <AiFillCheckCircle></AiFillCheckCircle> </h6>
-                            <h6> aaa</h6>
-                        </div>
-                        
+            <div className='bg-[#d3b0b0]'>
+
+                <div className='bg-[#FBFBFB] mx-auto'>
+                   <div className='mx-auto'>
+    
+                       <div className='flex mx-10 mt-4'>
+                        <AiFillCheckCircle className='text-[#0076a3] font-extrabold text-2xl mr-3'></AiFillCheckCircle>
+                        <h6 className='text-[#444] font-bold font-serif'> Instructor: 42.5% | Instructory: 57.5%</h6>
                     </div>
+                    <div className='flex mx-10 mt-4'>
+                        <AiFillCheckCircle className='text-[#0076a3] font-extrabold text-2xl mr-3'></AiFillCheckCircle>
+                        <h6 className='text-[#444] font-bold font-serif'>Social media announcement or teaser </h6>
+                    </div>
+                    <div className='flex mx-10 mt-4 text-left'>
+                        <AiFillCheckCircle className='text-[#0076a3] font-extrabold text-2xl mr-3 text-left'></AiFillCheckCircle>
+                        <h6 className='text-left text-[#444] font-bold font-serif'> Social media post</h6>
+                    </div>
+                    <div className='flex mx-10 mt-4'>
+                        <AiFillCheckCircle className='text-[#0076a3] font-extrabold text-2xl mr-3'></AiFillCheckCircle>
+                        <h6 className='text-[#444] font-bold font-serif'>We will do Paid marketing like FB Boost, Bulk SMS etc. </h6>
+                    </div>
+                    <div className='flex mx-10 mt-4'>
+                        <AiFillCheckCircle className='text-[#0076a3] font-extrabold text-2xl mr-3'></AiFillCheckCircle>
+                        <h6 className='text-[#444] font-bold font-serif'> promote to 50k+ audience</h6>
+                    </div><div className='flex mx-10 mt-4'>
+                        <AiFillCheckCircle className='text-[#0076a3] font-extrabold text-2xl mr-3'></AiFillCheckCircle>
+                        <h6 className='text-[#444] font-bold font-serif'>You can take Live session on our FB page/group </h6>
+                    </div>
+                    <div className='flex mx-10 mt-4'>
+                        <AiFillCheckCircle className='text-[#0076a3] font-extrabold text-2xl mr-3'></AiFillCheckCircle>
+                        <h6 className='text-[#444] font-bold font-serif'>Private group chat with Instructory team to get special support and updates </h6>
+                    </div>
+                    <div className='flex mx-10 mt-4'>
+                        <AiFillCheckCircle className='text-[#0076a3] font-extrabold text-2xl mr-3'></AiFillCheckCircle>
+                        <h6 className='text-[#444] font-bold font-serif'> We can make your teaser video if you can come to our office</h6>
+                    </div>
+                    <div className='flex mx-10 mt-4'>
+                        <AiFillCheckCircle className='text-[#0076a3] font-extrabold text-2xl mr-3'></AiFillCheckCircle>
+                        <h6 className='text-[#444] font-bold font-serif'> We will prioritize your course at our bigger events like competitions, festivals, admission fairs, etc. based on your activities.</h6>
+                    </div>
+                    
+                   </div>
                 </div>
             </div>
-
+            
         </div>
     );
 };
 
 export default Courseupload;
+
+
+
+
+{/* <div className='flex mx-10 mt-4'>
+<AiFillCheckCircle className='text-[#0076a3] font-extrabold text-2xl mr-3'></AiFillCheckCircle>
+<h6 className='text-[#444] font-bold font-serif'> Instructor: 42.5% | Instructory: 57.5%</h6>
+</div>
+<div className='flex mx-10 mt-4'>
+<AiFillCheckCircle className='text-[#0076a3] font-extrabold text-2xl mr-3'></AiFillCheckCircle>
+<h6 className='text-[#444] font-bold font-serif'>Social media announcement or teaser </h6>
+</div>
+<div className='flex mx-10 mt-4 text-left'>
+<AiFillCheckCircle className='text-[#0076a3] font-extrabold text-2xl mr-3 text-left'></AiFillCheckCircle>
+<h6 className='text-left text-[#444] font-bold font-serif'> Social media post</h6>
+</div>
+<div className='flex mx-10 mt-4'>
+<AiFillCheckCircle className='text-[#0076a3] font-extrabold text-2xl mr-3'></AiFillCheckCircle>
+<h6 className='text-[#444] font-bold font-serif'>We will do Paid marketing like FB Boost, Bulk SMS etc. </h6>
+</div>
+<div className='flex mx-10 mt-4'>
+<AiFillCheckCircle className='text-[#0076a3] font-extrabold text-2xl mr-3'></AiFillCheckCircle>
+<h6 className='text-[#444] font-bold font-serif'> promote to 50k+ audience</h6>
+</div><div className='flex mx-10 mt-4'>
+<AiFillCheckCircle className='text-[#0076a3] font-extrabold text-2xl mr-3'></AiFillCheckCircle>
+<h6 className='text-[#444] font-bold font-serif'>You can take Live session on our FB page/group </h6>
+</div>
+<div className='flex mx-10 mt-4'>
+<AiFillCheckCircle className='text-[#0076a3] font-extrabold text-2xl mr-3'></AiFillCheckCircle>
+<h6 className='text-[#444] font-bold font-serif'>Private group chat with Instructory team to get special support and updates </h6>
+</div>
+<div className='flex mx-10 mt-4'>
+<AiFillCheckCircle className='text-[#0076a3] font-extrabold text-2xl mr-3'></AiFillCheckCircle>
+<h6 className='text-[#444] font-bold font-serif'> We can make your teaser video if you can come to our office</h6>
+</div>
+<div className='flex mx-10 mt-4'>
+<AiFillCheckCircle className='text-[#0076a3] font-extrabold text-2xl mr-3'></AiFillCheckCircle>
+<h6 className='text-[#444] font-bold font-serif'> We will prioritize your course at our bigger events like competitions, festivals, admission fairs, etc. based on your activities.</h6>
+</div> */}
